b51d6592400de1822607e17621474dbe69c5cf48
Huytue <bachvonam1996@gmail.com>
2020-07-03 15:37:04 +0700

    chuyen doi ngon ngu

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,9 @@
 class OrdersController < ApplicationController
-  include CurrentCart
-  before_action :set_cart, only: [:new, :create]
-  before_action :ensure_cart_isnt_empty, only: :new
-  before_action :set_order, only: [:show, :edit, :update, :destroy]
+    skip_before_action :authorize
+    include CurrentCart
+    before_action :set_cart, only: [:new, :create]
+    before_action :ensure_cart_isnt_empty, only: :new
+    before_action :set_order, only: [:show, :edit, :update, :destroy]
 
   # GET /orders
 
